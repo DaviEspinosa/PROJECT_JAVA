928d2c20f2d594adf3a7894815f55b2ea3989ed4
Alberto-MeloL <albertomeloteco@gmail.com>
2023-12-12 15:07:48 -0300

    12/12/2023

--- Super-Mercado/src/Controller/CadastrarProdutosControll.java
+++ Super-Mercado/src/Controller/CadastrarProdutosControll.java
@@ -1,47 +1,47 @@
-package Controller;
+// package Controller;
 
-import java.awt.event.FocusListener;
-import java.sql.SQLException;
-import java.util.List;
-import java.awt.event.FocusEvent;
+// import java.awt.event.FocusListener;
+// import java.sql.SQLException;
+// import java.util.List;
+// import java.awt.event.FocusEvent;
 
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.table.DefaultTableModel;
+// import javax.swing.JTable;
+// import javax.swing.JTextField;
+// import javax.swing.table.DefaultTableModel;
  
-import Connection.ConnectionFactory;
-import DAO.CadastrarProdutosDAO;
-import Model.Produtos;
+// import Connection.ConnectionFactory;
+// import DAO.CadastrarProdutosDAO;
+// import Model.Produtos;
 
-public class CadastrarProdutosControll {
+// public class CadastrarProdutosControll {
 
-    private List<Produtos> produtoList;
-    private JTable table;
-    private DefaultTableModel tableModel;
+//     private List<Produtos> produtoList;
+//     private JTable table;
+//     private DefaultTableModel tableModel;
     
-    // PlaceHolder
-    public JTextField createTextFieldWithPlaceholderProdutos(String placeholder){
-        JTextField textFieldProdutos = new JTextField(placeholder);
-        textFieldProdutos.addFocusListener(new FocusListener() {
-            @Override
-            public void focusGained(FocusEvent e){
-                if (textFieldProdutos.getText().equals(placeholder)) {
-                    textFieldProdutos.setText("");
-                }
-            }
-            @Override
-            public void focusLost(FocusEvent e){
-                if (textFieldProdutos.getText().isEmpty()) {
-                    textFieldProdutos.setText(placeholder);
-                }
-            }
-        });
-        return textFieldProdutos;
-    }
+//     // PlaceHolder
+//     public JTextField createTextFieldWithPlaceholderProdutos(String placeholder){
+//         JTextField textFieldProdutos = new JTextField(placeholder);
+//         textFieldProdutos.addFocusListener(new FocusListener() {
+//             @Override
+//             public void focusGained(FocusEvent e){
+//                 if (textFieldProdutos.getText().equals(placeholder)) {
+//                     textFieldProdutos.setText("");
+//                 }
+//             }
+//             @Override
+//             public void focusLost(FocusEvent e){
+//                 if (textFieldProdutos.getText().isEmpty()) {
+//                     textFieldProdutos.setText(placeholder);
+//                 }
+//             }
+//         });
+//         return textFieldProdutos;
+//     }
 
-    public void cadastrar(String nome, String preco, String quantidade, String codigoBarra){
-        new CadastrarProdutosDAO().cadastrar(nome,preco,quantidade,codigoBarra);
-    }
+//     public void cadastrar(String nome, String preco, String quantidade, String codigoBarra){
+//         new CadastrarProdutosDAO().cadastrar(nome,preco,quantidade,codigoBarra);
+//     }
     
 
-}
+// }
